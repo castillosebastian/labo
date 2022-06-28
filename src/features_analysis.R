1c5ef4b54dc498d862ffd66f5a9164f7af9376a5
sebacastillo <castilloclaudiosebastian@gmail.com>
2022-06-28 20:38:04 -0300

    feature analysis

--- src/features_analysis.R
+++ src/features_analysis.R
@@ -1,14 +1,34 @@
-library(purrr)
 library(data.table)
+library(dplyr) 
+library(stringr)
+library(ggplot2)
 
-setwd("~/R/labo/exp/HT7310")
+data_dir = setwd("~/R/labo/exp/archivo")
 
-tbl_fread <- 
-  list.files(pattern = "*.csv") %>% 
-  map_df(~fread(.))
+filenames <- list.files(pattern="impo_*", recursive = T)
 
-features_summary <- tbl_fread %>% 
+tbl <- do.call(rbind, 
+               lapply(filenames, function(x) 
+                 cbind(fread(x, stringsAsFactors = FALSE), filename = x)))
+tbl = tbl %>% 
+  mutate(experimento = str_sub(filename, 1, 7),
+         top_modelos = as.integer(str_sub(filename, 14,15))) 
+        
+features_summary <- tbl %>% 
   group_by(Feature) %>% 
-  summarise(sum_gain = sum(Gain, na.rm = T),
+  summarise(ganancia_media = mean(Gain, na.rm = T),
+            ganancia_total = sum(Gain, na.rm = T),
+            ganancia_max = max(Gain, na.rm = T),
+            ganancia_min = min(Gain, na.rm = T),
+            ganancia_sd = round(sd(Gain, na.rm = T), digits = 2),
+            posicion_promedio = round(median(pos, na.rm=T)),
             ocurrencias = n()) %>% 
-  arrange(desc(sum_gain))
+  arrange(desc(ganancia_media))
+
+
+features_summary %>% 
+  filter(posicion_promedio < 20 & !str_detect(Feature, "ctrx_quart")) %>% 
+  ggplot2::ggplot(aes(y=ganancia_media, x=posicion_promedio, label=Feature)) +
+  geom_point() + 
+  ggrepel::geom_label_repel()
+  
