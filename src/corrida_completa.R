0428ed4c267310a84e45541fe9c72ac16ad14da0
sebacastillo <castilloclaudiosebastian@gmail.com>
2022-07-11 10:12:36 -0300

    .

--- src/corrida_completa.R
+++ src/corrida_completa.R
@@ -1,7 +1,7 @@
 # corrida completa
-exp_start( "8121TSb",  deletevm= FALSE)
-exp_start( "8121HTb",  deletevm= FALSE)
-exp_start( "8121ZZb")
+# exp_start( "8121TSb",  deletevm= FALSE)
+# exp_start( "8121HTb",  deletevm= FALSE)
+# exp_start( "8121ZZb")
 # Corrida completa
 exp_start( "8121TSc",  deletevm= FALSE)
 exp_start( "8121HTc",  deletevm= FALSE)
